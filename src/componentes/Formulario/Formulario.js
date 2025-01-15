ab139e60bca1698083cb9306066b5987d76bcdf8
unknown <diegoa.zunigam@uqvirtual.edu.co>
2025-01-15 16:16:41 -0500

    Creacion del componente Equipo y su respectivo funcionamiento

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -4,7 +4,7 @@ import CampoTexto from "../CampoTexto"
 import ListaOpciones from "../ListaOpciones"
 import Boton from "../Boton"
 
-const Formulario = () => {
+const Formulario = (props) => {
 
     const [nombre, actualizarNombre] = useState("")
     const [puesto, actualizarPuesto] = useState("")
@@ -50,7 +50,8 @@ const Formulario = () => {
             />
             <ListaOpciones 
                 valor={equipo} 
-                actualizarEquipo={actualizarEquipo}     
+                actualizarEquipo={actualizarEquipo} 
+                equipos={props.equipos}    
             />
             <Boton>
                 Crear
