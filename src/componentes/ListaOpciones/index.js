d9b43a1a19bf4903d90abc8ee80fc21ccf147654
unknown <diegoa.zunigam@uqvirtual.edu.co>
2025-01-15 15:13:55 -0500

    Subiendo proyecto a GitHub

---
+++ src/componentes/ListaOpciones/index.js
@@ -0,0 +1,32 @@
+import "./ListaOpciones.css"
+
+const ListaOpciones = (props) =>{
+
+    //Metodo map -> arreglo.map( (equipo, index)=> {
+    //   return <option></option>
+    //} )
+    const equipos = [
+        "Programación",
+        "Front End",
+        "Data Science",
+        "Devops",
+        "UX y Diseño",
+        "Móvil",
+        "Innovacion y Gestión"
+    ]
+
+    const manejarCambio = (e) => {
+        console.log("cambio", e.target.value)
+        props.actualizarEquipo(e.target.value)
+    }
+
+    return <div className="lista-opciones">
+        <label>Equipos</label>
+        <select value={props.valor} onChange={manejarCambio}>
+            <option value="" disabled defaultValue="" hidden>Seleccionar equipo</option>
+            { equipos.map( (equipo, index) => <option key={index} value={equipo}>{equipo}</option>)}
+        </select>
+    </div>
+}
+
+export default ListaOpciones;
