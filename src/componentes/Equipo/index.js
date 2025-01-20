e2bef3e01fa457232f45bb55cda80c458d95fdf0
unknown <diegoa.zunigam@uqvirtual.edu.co>
2025-01-20 13:30:36 -0500

    Se completa el proyecto

--- src/componentes/Equipo/index.js
+++ src/componentes/Equipo/index.js
@@ -1,14 +1,16 @@
 import "./Equipo.css"
 import Colaborador from "../Colaborador"
+import hexToRgba from "hex-to-rgba"
 
 const Equipo = (props) => {
     //Destructuracion
-    const { colorPrimario, colorSecundario, titulo } = props.datos
-    const { colaboradores } = props
+    const { colorPrimario, colorSecundario, titulo, id } = props.datos
+    const { colaboradores, eliminarColaborador, actualizarColor, like } = props
     const obj = {
-        backgroundColor: colorSecundario
+        backgroundColor: hexToRgba(colorPrimario, 0.6)
     }
-    console.log(colaboradores.length > 0)
+    
+    
 
     const estiloTitulo = {borderColor: colorPrimario }
 
@@ -16,13 +18,24 @@ const Equipo = (props) => {
         {
             colaboradores.length > 0 &&
                 <section className="equipo" style={obj} >
+                    <input 
+                        type='color'
+                        className="input-color"
+                        value={hexToRgba(colorPrimario, 0.6)}
+                        onChange={(e) => {
+                            actualizarColor(e.target.value, id)
+                        }}
+                    />
                     <h3 style={estiloTitulo}>{titulo}</h3>
                     <div className="colaboradores">
                         {
                             colaboradores.map((colaborador, index) => <Colaborador 
                                 datos={colaborador} 
                                 key={index} 
-                                colorPrimario={colorPrimario} />)
+                                colorPrimario={colorPrimario} 
+                                eliminarColaborador={eliminarColaborador}
+                                like={like}
+                                />)
                         }
                     </div>
                 </section>
